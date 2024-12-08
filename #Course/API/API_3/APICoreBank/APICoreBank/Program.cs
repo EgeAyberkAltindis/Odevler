9455c968416d95ef1f2fa170b8c6d561d21ed699
ayberk.altindis <ayberk.altindis@KDK-404-PCO4-YZ>
2024-12-08 12:14:32 +0300

    .

--- #Course/API/API_3/APICoreBank/APICoreBank/Program.cs
+++ #Course/API/API_3/APICoreBank/APICoreBank/Program.cs
@@ -1,3 +1,6 @@
+using APICoreBank.Models.ContextClasses;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContextPool<MyContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("MyConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
